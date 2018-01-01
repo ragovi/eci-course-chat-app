772726d6e115c924a6f5425e28fe5d12d2d0333d
Raúl González Vicente <rgonzale@viewnext.com>
2018-01-01 13:02:05 +0100

    Emit message on createMessage

--- server/server.js
+++ server/server.js
@@ -18,14 +18,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'Hello!',
-    createdAt: 123
-  });
+
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    // io.emit emite un mensaje para todas las conexiones, todos los sockets abiertos
+    // socket.emit lo hace para una sola conexion
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
