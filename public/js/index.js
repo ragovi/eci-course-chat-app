c26f9c63be301d5cdd3d2bdc572c550193ea833d
Raúl González Vicente <rgonzale@viewnext.com>
2018-01-02 13:09:09 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -36,11 +36,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(event) {
   event.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-    // ack
+    messageTextbox.val('');
   });
 });
 
@@ -50,12 +52,17 @@ locationButton.on('click', () => {
     return alert('Geolocation not supporrted by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
-  })
+  });
 });
